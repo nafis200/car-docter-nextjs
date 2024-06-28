34d22ec6ae0d2b8e9122dde905aa1bd63042d74d
Nafis Ahamed <nafisahamed14@gmail.com>
2024-06-28 22:47:59 +0600

    services

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,4 +1,5 @@
 import Banner from "./Banner";
+import Services from "./Services";
 
 
 
@@ -6,6 +7,7 @@ const Homepage = () => {
     return (
         <div>
           <Banner></Banner>
+          <Services></Services>
         </div>
     );
 };
